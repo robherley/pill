e66faacdbde24bee4e9b3a426ec072ce02411584
Robert Herley <rherley@stevens.edu>
2017-08-22 22:25:51 -0400

    finished video navigation, misc ui changes

--- src/components/Left.jsx
+++ src/components/Left.jsx
@@ -1,15 +1,17 @@
 import React, { Component } from "react";
 import ReactPlayer from "react-player";
 import styled from "styled-components";
+import { Textfit } from "react-textfit";
 import { connect } from "react-redux";
-import { changeVolume, changeVideoIndex, pausePlay } from '../actions/';
+import { changeVolume, changeVideoIndex, pausePlay } from "../actions/";
 
 import {
   Box,
+  Button,
   PlayIcon,
   PauseIcon,
-  FastForwardIcon,
-  RewindIcon,
+  FormPreviousIcon,
+  FormNextIcon,
   VolumeIcon,
   VolumeLowIcon,
   VolumeMuteIcon
@@ -19,68 +21,98 @@ const FlexRow = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-around;
-  padding-top: 25px;
   margin: 0px 40px;
 `;
 
-const Input = styled.input`
-  padding-left: 20px;
+const StyledBox = styled(Box)`
+  width: 350px;
+  padding: 3px;
+  margin: 5px;
+  text-align: center;
 `;
 
+const Input = styled.input`padding-left: 20px;`;
+
 class Left extends Component {
-  getSafeID(newID){
-    if(newID < 0){
-      this.props.changeVideoIndex(this.props.playlist.items.length - 1)
-    }
-    else if(newID > this.props.playlist.items.length){
-      this.props.changeVideoIndex(0)
-    }
-    else{
-      this.props.changeVideoIndex(newID)
+  getSafeID(newID) {
+    if (newID < 0) {
+      this.props.changeVideoIndex(this.props.playlist.items.length - 1);
+    } else if (newID > this.props.playlist.items.length) {
+      this.props.changeVideoIndex(0);
+    } else {
+      this.props.changeVideoIndex(newID);
     }
   }
 
-  getVolumeIcon(){
+  getVolumeIcon() {
     const vol = parseFloat(this.props.volume);
-    if(vol === 0){
-      return <VolumeMuteIcon size='small' />
-    }
-    else if(vol < 0.6){
-      return <VolumeLowIcon size='small' />
-    }
-    else {
-      return <VolumeIcon size='small' />
+    if (vol === 0) {
+      return <VolumeMuteIcon size="small" />;
+    } else if (vol < 0.5) {
+      return <VolumeLowIcon size="small" />;
+    } else {
+      return <VolumeIcon size="small" />;
     }
   }
 
   render() {
-    const YT_VIDEO_ID = this.props.playlist.items[this.props.video.index].snippet.resourceId.videoId;
+    const YT_VIDEO_ID = this.props.playlist.items[this.props.video.index]
+      .snippet.resourceId.videoId;
+    const vidInfo = this.props.playlist.items[this.props.video.index].snippet;
+    console.log(vidInfo);
     return (
       <div>
-        <ReactPlayer
-          url={`https://www.youtube.com/watch?v=${YT_VIDEO_ID}`}
-          playing={this.props.video.playing}
-          preload
-          width={320}
-          height={180}
-          onEnded={() => this.getSafeID(this.props.video.index + 1)}
-          volume={parseFloat(this.props.volume)}
-          style={{
-            borderRadius: "10px",
-            overflow: "hidden",
-            pointerEvents: "none"
-          }}
-        />
         <FlexRow>
-          <RewindIcon onClick={() => this.getSafeID(this.props.video.index - 1)}/>
-          {this.props.video.playing? 
-            <PauseIcon size="large" onClick={() => this.props.pausePlay()}/> : 
-            <PlayIcon size="large" onClick={() => this.props.pausePlay()}/>}
-          <FastForwardIcon onClick={() => this.getSafeID(this.props.video.index + 1)}/>
+          <ReactPlayer
+            url={`https://www.youtube.com/watch?v=${YT_VIDEO_ID}`}
+            playing={this.props.video.playing}
+            preload
+            width={320}
+            height={180}
+            onEnded={() => this.getSafeID(this.props.video.index + 1)}
+            volume={parseFloat(this.props.volume)}
+            style={{
+              marginTop: "40px",
+              borderRadius: "10px",
+              overflow: "hidden",
+              pointerEvents: "none"
+            }}
+          />
         </FlexRow>
-        <Box direction="row" justify="between" pad="medium">
+        <FlexRow>
+          <StyledBox >
+            <Textfit mode="multi" style={{opacity: '0.7'}}>
+              {vidInfo.title}
+            </Textfit>
+          </StyledBox>
+        </FlexRow>
+        <FlexRow>
+          <Button
+            plain
+            icon={<FormPreviousIcon size="large" />}
+            onClick={() => this.getSafeID(this.props.video.index - 1)}
+          />
+          <Button
+            plain
+            icon={this.props.video.playing? <PauseIcon size="large" /> : <PlayIcon size="large" />}
+            onClick={() => this.props.pausePlay()}
+          />
+          <Button
+            plain
+            icon={<FormNextIcon size="large" />}
+            onClick={() => this.getSafeID(this.props.video.index + 1)}
+          />
+        </FlexRow>
+        <Box direction="row" justify="between" style={{ padding: "5px 70px" }}>
           {this.getVolumeIcon()}
-          <Input type="range" min='0' max='1' step='0.1' value={this.props.volume} onChange={e => this.props.changeVolume(e.target.value)} />
+          <Input
+            type="range"
+            min="0"
+            max="1"
+            step="0.1"
+            value={this.props.volume}
+            onChange={e => this.props.changeVolume(e.target.value)}
+          />
         </Box>
       </div>
     );
@@ -93,4 +125,8 @@ const mapStateToProps = state => ({
   volume: state.volume
 });
 
-export default connect(mapStateToProps, { changeVolume, changeVideoIndex, pausePlay })(Left);
+export default connect(mapStateToProps, {
+  changeVolume,
+  changeVideoIndex,
+  pausePlay
+})(Left);
--- src/components/Right.jsx
+++ src/components/Right.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import styled from "styled-components";
 import _ from 'lodash';
 import { connect } from "react-redux";
+import { changeVideoIndex } from '../actions/';
 import {
   List,
   ListItem,
@@ -23,15 +24,29 @@ const BoxFooter = styled(Box)`
 const SongTitle = styled.span`
   font-size: 14px;
   font-weight: 300;
+`
+
+const StyledListItem = styled(ListItem)`
+  opacity: ${props => props.selected? '1' : '0.5'
+  };
+
   &:hover{
-    font-weight: 400;
+    background-color: rgba(255,255,255,0.1)
   }
 `
 
 class Right extends Component {
+  renderListItem(index, title){
+    return (
+      <StyledListItem selected={index === this.props.video.index} justify="between" key={index} onClick={() => this.props.changeVideoIndex(index)} separator={index === this.props.video.index ? "horizontal" : 'none'}>
+        <SongTitle>{title}</SongTitle>
+      </StyledListItem>
+    )
+  }
+
   render() {
     const { items } = this.props.playlist;
-    let videoIndex = 1;
+    let videoIndex = 0;
     return (
       <div>
         <Header style={{ height: "20vh" }} justify="center">
@@ -43,6 +58,7 @@ class Right extends Component {
           >
             <Title style={{ marginLeft: "10px" }}>Proximity Uploads</Title>
             <Button
+              plain
               icon={<ActionsIcon />}
               onClick={() => console.log("hi")}
             />
@@ -50,15 +66,8 @@ class Right extends Component {
         </Header>
         <List style={{ height: "70vh" }}>
            {_.map(items, item => 
-           videoIndex === 1?
-            <ListItem justify="between" key={++videoIndex} separator="horizontal">
-              <SongTitle>{item.snippet.title}</SongTitle>
-            </ListItem>
-            :
-            <ListItem justify="between" key={++videoIndex}>
-              <SongTitle>{item.snippet.title}</SongTitle>
-            </ListItem>
-          )} 
+            this.renderListItem(videoIndex++, item.snippet.title)
+           )}
         </List>
         <Footer style={{ height: "10vh" }} justify="center">
           <BoxFooter colorIndex="neutral-4">
@@ -72,7 +81,8 @@ class Right extends Component {
 }
 
 const mapStateToProps = state => ({
-  playlist: state.playlist
+  playlist: state.playlist,
+  video: state.video
 })
 
-export default connect(mapStateToProps, null)(Right);
+export default connect(mapStateToProps, { changeVideoIndex })(Right);
